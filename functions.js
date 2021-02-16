edb89e3433fc646b2bde5718a8bae50c799bff7a
kylehansen17 <kyle.hansen925@gmail.com>
2021-02-16 08:00:49 -0600

    Simple Functions

---
+++ functions.js
@@ -0,0 +1,34 @@
+// function funcName() {
+
+// }
+
+
+function singSong() {
+    console.log('DO');
+    console.log('RE');
+    console.log('MI');
+
+}
+singSong()
+
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${fistName} ${lastName[0]}.`);
+}
+
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
